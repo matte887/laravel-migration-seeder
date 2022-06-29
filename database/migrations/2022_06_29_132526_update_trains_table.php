c0d296363779ee3892009994ee1848e75da0ef3b
matte887 <m.erroi92@gmail.com>
2022-06-29 15:33:49 +0200

    Aggiunte colonne con update

---
+++ database/migrations/2022_06_29_132526_update_trains_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->tinyInteger('binario_partenza')->after('stazione_partenza');
+            $table->tinyInteger('binario_arrivo')->after('stazione_arrivo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('binario_partenza');
+            $table->dropColumn('binario_arrivo');
+        });
+    }
+}
